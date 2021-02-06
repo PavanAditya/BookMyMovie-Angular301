0bb040e8c36bbccf49519f166b9d5a0df95ea4e1
Pavan Aditya M S <pavanaditya.ms@stupify.in>
2021-02-06 22:43:22 +0530

    redirects and json server url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_KEY: 'api_key=c4f351490cb39722773a30b3347f2e0d',
-  // JSONSERVER: 'https://book-my-movie-data.firebaseapp.com/api'
+  // JSONSERVER: 'https://bookmymovie.pavanaditya.com/api'
   JSONSERVER: 'http://localhost:3000'
 };
